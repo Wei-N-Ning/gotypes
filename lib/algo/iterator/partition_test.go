31ea58a331902c140cbcfad2c954d678885d9ca0
Wei-N-Ning <macgnw@gmail.com>
2022-02-06 10:55:56 +1100

    partitioned

---
+++ lib/algo/iterator/partition_test.go
@@ -0,0 +1,20 @@
+package iterator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPartitionExpectBothEmpty(t *testing.T) {
+	l, r := Partitioned(Empty[int](), func(x int) bool { return x%2 == 1 })
+	assert.False(t, l.Next().IsSome())
+	assert.False(t, r.Next().IsSome())
+}
+
+func TestPartitionExpectOrder(t *testing.T) {
+	l, r := Partitioned(Range(1, 100), func(x int) bool { return x%2 == 1 })
+	xs := l.Take(4).Slice()
+	ys := r.Take(4).Slice()
+	assert.Equal(t, []int{1, 3, 5, 7}, xs)
+	assert.Equal(t, []int{2, 4, 6, 8}, ys)
+}
