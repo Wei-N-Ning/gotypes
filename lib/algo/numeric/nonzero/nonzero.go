cf2d10bff3c609a4632baad7caa06d5e8c072939
Wei-N-Ning <macgnw@gmail.com>
2022-02-12 11:10:20 +1100

    more numeric

--- lib/algo/numeric/nonzero/nonzero.go
+++ lib/algo/numeric/nonzero/nonzero.go
@@ -1,6 +1,7 @@
-package numeric
+package nonzero
 
 import "constraints"
+import "go-types-nw/lib/algo/option"
 
 type NonZero[T constraints.Float | constraints.Integer] struct {
 	x T
@@ -15,4 +16,11 @@ func UnsafeNewValue[T constraints.Float | constraints.Integer](x T) NonZero[T] {
 		panic(x)
 	}
 	return NonZero[T]{x: x}
+}
+
+func TryNewValue[T constraints.Float | constraints.Integer](x T) option.Option[NonZero[T]] {
+	if x == 0 {
+		return option.None[NonZero[T]]()
+	}
+	return option.Some(NonZero[T]{x: x})
 }
---
+++ lib/algo/numeric/nonzero/nonzero_test.go
@@ -0,0 +1,23 @@
+package nonzero
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNonZeroExpectOptional(t *testing.T) {
+	assert.False(t, TryNewValue(0).IsSome())
+	assert.True(t, TryNewValue(1).IsSome())
+	assert.Equal(t, -1, UnsafeNewValue(-1).Unwrap())
+}
+
+func TestEnforceTypeCompliance(t *testing.T) {
+	// compile error:
+	// string does not implement constraints.Float|constraints.Integer
+	//val := TryNewValue("a")
+	//assert.False(t, val.IsSome())
+
+	assert.False(t, TryNewValue(0.0).IsSome())
+	assert.True(t, TryNewValue(0.0000001).IsSome())
+	assert.True(t, TryNewValue(-0.0000001).IsSome())
+}
