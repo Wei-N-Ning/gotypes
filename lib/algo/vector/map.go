b3986db5eb27edebf18605aab5620002f418bda0
Wei-N-Ning <macgnw@gmail.com>
2022-02-12 20:57:04 +1100

    more vec

---
+++ lib/algo/vector/map.go
@@ -0,0 +1,10 @@
+package vector
+
+func Map[T any, R any](vec *Vector[T], f func(T) R) Vector[R] {
+	newVec := WithCapacity[R](vec.Capacity())
+	for idx := 0; idx < vec.size; idx++ {
+		newVec.xs[idx] = f(vec.xs[idx])
+	}
+	newVec.size = vec.size
+	return newVec
+}
---
+++ lib/algo/vector/map_test.go
@@ -0,0 +1,18 @@
+package vector
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMapExpectSizeCapacity(t *testing.T) {
+	v1 := FromValues("e1", "e1m1", "map:e1m1")
+	v2 := Map(&v1, func(x string) int { return len(x) })
+	assert.Equal(t, v1.Capacity(), v2.Capacity())
+	assert.Equal(t, v1.Size(), v2.Size())
+
+	assert.Equal(t, 8, v2.TryPop().Unwrap())
+	assert.Equal(t, 4, v2.TryPop().Unwrap())
+	assert.Equal(t, 2, v2.TryPop().Unwrap())
+	assert.False(t, v2.TryPop().IsSome())
+}
