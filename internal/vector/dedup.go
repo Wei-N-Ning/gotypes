b67b8d4268c84f41ebe493aa0ca6076aeea88d39
Wei-N-Ning <macgnw@gmail.com>
2022-03-09 21:46:53 +1100

    more vector

--- internal/vector/dedup.go
+++ internal/vector/dedup.go
@@ -35,3 +35,61 @@ func DedupInplace[T comparable](vec *Vector[T]) {
 	})
 	vec.size = lastIdx + 1
 }
+
+func DedupBy[T any](vec Vector[T], cmp func(lhs T, rhs T) bool) Vector[T] {
+	if vec.Size() < 2 {
+		return vec
+	}
+	newVec := WithCapacity[T](vec.Capacity() - 1)
+	newVec.Push(vec.Head().Unwrap())
+	return Fold2[T, Vector[T]](vec.Tail(), newVec, func(v Vector[T], elem T) Vector[T] {
+		if !cmp(elem, v.Back()) {
+			newVec.Push(elem)
+		}
+		return newVec
+	})
+}
+
+func DedupInplaceBy[T any](vec *Vector[T], cmp func(lhs T, rhs T) bool) {
+	if vec.Size() < 2 {
+		return
+	}
+	lastIdx := Fold2[T, int](vec.Tail(), 0, func(prevIdx int, elem T) int {
+		if cmp(vec.Get(prevIdx), elem) {
+			return prevIdx
+		}
+		prevIdx++
+		vec.Set(prevIdx, elem)
+		return prevIdx
+	})
+	vec.size = lastIdx + 1
+}
+
+func DedupByKey[T any, K comparable](vec Vector[T], key func(T) K) Vector[T] {
+	if vec.Size() < 2 {
+		return vec
+	}
+	newVec := WithCapacity[T](vec.Capacity() - 1)
+	newVec.Push(vec.Head().Unwrap())
+	return Fold2[T, Vector[T]](vec.Tail(), newVec, func(v Vector[T], elem T) Vector[T] {
+		if key(elem) != key(v.Back()) {
+			newVec.Push(elem)
+		}
+		return newVec
+	})
+}
+
+func DedupInplaceByKey[T any, K comparable](vec *Vector[T], key func(T) K) {
+	if vec.Size() < 2 {
+		return
+	}
+	lastIdx := Fold2[T, int](vec.Tail(), 0, func(prevIdx int, elem T) int {
+		if key(vec.Get(prevIdx)) == key(elem) {
+			return prevIdx
+		}
+		prevIdx++
+		vec.Set(prevIdx, elem)
+		return prevIdx
+	})
+	vec.size = lastIdx + 1
+}
--- internal/vector/dedup_test.go
+++ internal/vector/dedup_test.go
@@ -20,7 +20,43 @@ func TestDedup(t *testing.T) {
 	})
 }
 
-func TestDedupIndex(t *testing.T) {
+func TestDedupBy(t *testing.T) {
+	cmp := func(lhs string, rhs string) bool {
+		return lhs[:min(2, len(lhs))] == rhs[:min(2, len(rhs))]
+	}
+	t.Run("empty vector", func(t *testing.T) {
+		vs := FromValues[string]()
+		dvs := DedupBy(vs, cmp)
+		assert.Equal(t, []string{}, dvs.ToSlice())
+	})
+	t.Run("expect elements removed", func(t *testing.T) {
+		assert.Equal(t, []string{"a", "idkfa", "b"}, DedupBy(FromValues("a", "idkfa", "idfa", "b"), cmp).ToSlice())
+		assert.Equal(t, []string{"id"}, DedupBy(FromValues("id", "idnoclip", "iddqd", "idkfa", "idfa"), cmp).ToSlice())
+	})
+	t.Run("expect no effect", func(t *testing.T) {
+		assert.Equal(t, []string{"a", "b", "c"}, DedupBy(FromValues("a", "b", "c"), cmp).ToSlice())
+	})
+}
+
+func TestDedupByKey(t *testing.T) {
+	keyF := func(x string) int {
+		return len(x)
+	}
+	t.Run("empty vector", func(t *testing.T) {
+		vs := FromValues[string]()
+		dvs := DedupByKey(vs, keyF)
+		assert.Equal(t, []string{}, dvs.ToSlice())
+	})
+	t.Run("expect elements removed", func(t *testing.T) {
+		assert.Equal(t, []string{"1", "e1m1", "b"}, DedupByKey(FromValues("1", "a", "e1m1", "idfa", "b", "_"), keyF).ToSlice())
+		assert.Equal(t, []string{"id"}, DedupByKey(FromValues("id", "e1", "m1", "zz", "op"), keyF).ToSlice())
+	})
+	t.Run("expect no effect", func(t *testing.T) {
+		assert.Equal(t, []string{"a1", "b12", "c"}, DedupByKey(FromValues("a1", "b12", "c"), keyF).ToSlice())
+	})
+}
+
+func TestDedupInplace(t *testing.T) {
 	t.Run("empty vector", func(t *testing.T) {
 		vs := FromValues[int]()
 		DedupInplace(&vs)
@@ -34,6 +70,45 @@ func TestDedupIndex(t *testing.T) {
 	t.Run("expect elements reduced to singleton", func(t *testing.T) {
 		vs := FromValues[int](1, 1, 1, 1, 1, 1, 1, 1)
 		DedupInplace(&vs)
+		assert.Equal(t, []int{1}, vs.ToSlice())
+	})
+}
+
+func TestDedupInplaceBy(t *testing.T) {
+	cmp := func(lhs string, rhs string) bool {
+		return lhs[:min(2, len(lhs))] == rhs[:min(2, len(rhs))]
+	}
+	t.Run("empty vector", func(t *testing.T) {
+		vs := FromValues[string]()
+		DedupInplaceBy(&vs, cmp)
+		assert.Equal(t, []string{}, vs.ToSlice())
+	})
+	t.Run("expect elements removed", func(t *testing.T) {
+		vs := FromValues("aab", "aa", "idkfa", "idfa", "bl", "bliz")
+		DedupInplaceBy(&vs, cmp)
+		assert.Equal(t, []string{"aab", "idkfa", "bl"}, vs.ToSlice())
+	})
+	t.Run("expect elements reduced to singleton", func(t *testing.T) {
+		vs := FromValues[int](1, 1, 1, 1, 1, 1, 1, 1)
+		DedupInplaceBy(&vs, func(x int, y int) bool { return x == y })
+		assert.Equal(t, []int{1}, vs.ToSlice())
+	})
+}
+
+func TestDedupInplaceByKey(t *testing.T) {
+	t.Run("empty vector", func(t *testing.T) {
+		vs := FromValues[string]()
+		DedupInplaceByKey(&vs, func(string) int { return 0 })
+		assert.Equal(t, []string{}, vs.ToSlice())
+	})
+	t.Run("expect elements removed", func(t *testing.T) {
+		vs := FromValues("a_", "aa", "e1m1", "idfa", "e1m4", "bliz")
+		DedupInplaceByKey(&vs, func(x string) int { return len(x) })
+		assert.Equal(t, []string{"a_", "e1m1"}, vs.ToSlice())
+	})
+	t.Run("expect elements reduced to singleton", func(t *testing.T) {
+		vs := FromValues[int](1, 1, 1, 1, 1, 1, 1, 1)
+		DedupInplaceByKey(&vs, func(x int) bool { return x == 10 })
 		assert.Equal(t, []int{1}, vs.ToSlice())
 	})
 }
