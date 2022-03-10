9507387191b265d335656b3c8cee8ee8bc10f038
Wei-N-Ning <macgnw@gmail.com>
2022-03-10 13:10:25 +1100

    more sumtype

--- internal/enum/sumtype_test.go
+++ internal/enum/sumtype_test.go
@@ -7,6 +7,9 @@ import (
 
 func TestSumtypeConstraint(t *testing.T) {
 	assert.Equal(t, 1, ErrorCode(InvalidPassword{}))
+	assert.Equal(t, 2, ErrorCode(NotFound{}))
+	assert.Equal(t, 3, ErrorCode(Timeout{}))
+	assert.Equal(t, 4, ErrorCode(InternalError{}))
 
 	// won't compile: error does not implement ProdError
 	//ErrorCode(fmt.Errorf("won't work"))
