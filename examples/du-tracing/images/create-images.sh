d4eb528541e61373a3d9cb9588746273a51e01fb
Wei-N-Ning <macgnw@gmail.com>
2022-03-22 20:18:52 +1100

    test data

---
+++ examples/du-tracing/images/create-images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# generate 100 16Mb mock image files
+
+# usage:
+# cd images
+# bash ./create-images.sh
+
+set -euo pipefail
+thisdir=$( cd "$(dirname "$0")" || exit 1; pwd )
+for idx in $(seq 1 100); do
+  dd if=/dev/urandom of="${thisdir}/image_${idx}.bin" bs=1024x1024 count=16
+done
